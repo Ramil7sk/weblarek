66eae8a3779161df21cabd3689895c941a959b53
Ramil Jarmukhametov <Ramil1986@yandex.ru>
2026-01-16 21:26:40 +0500

    внес изменения в main.js

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { ApiService } from "./components/models/APIservice";
 const productsModel = new Products();
 productsModel.saveProducts(apiProducts.items);
 console.log("Массив товаров из каталога:", productsModel.getProducts());
+console.log("Получение одного товара по id:", productsModel.getProductById("854cef69-976d-4c2a-a18c-2aa45046c390"));
+productsModel.saveSelectedProduct(apiProducts.items[0]);
+console.log("Выбранный товар:", productsModel.getSelectedProduct());
+
 
 const api = new Api("https://larek-api.nomoreparties.co/api/weblarek");
 const apiService = new ApiService(api);
