e653e1bf7bcd9a3f664dea18564acaeca60bff84
Ramil Jarmukhametov <Ramil1986@yandex.ru>
2026-01-16 17:21:55 +0500

    провел проверку с моковыми данными

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import './scss/styles.scss';
+import { Products } from './components/models/Products';
+import { apiProducts } from './utils/data';
+
+const productsModel = new Products();
+productsModel.saveProducts(apiProducts.items); 
+console.log('Массив товаров из каталога:' , productsModel.getProducts())
